cf0f03ba7f46f5a98869bd097c424eb7f3077f29
Levi Schubert <me@levischubert.com>
2025-12-05 11:16:20 -0600

    prep day 6 files

---
+++ src/Solutions/DaySix.kt
@@ -0,0 +1,11 @@
+package Solutions
+
+class DaySix : Solver {
+    override fun partOne(lines: List<String>): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun partTwo(lines: List<String>): String {
+        TODO("Not yet implemented")
+    }
+}
